69219d4d78aa353332b91c925ec37a86d5656536
jsnstdt <jan-sebastian.nystedt@student.kpedu.fi>
2017-04-05 15:46:55 +0300

    Sprites, scripts

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour {
+
+    public Texture enemyType1Texture;
+    public Texture enemyType2Texture;
+
+    void OnGUI()
+    {
+
+        if (1 < 2 && GUI.Button(new Rect(960, 10, 50, 50), enemyType1Texture))
+            Debug.Log("Clicked");
+        if (1 < 2 && GUI.Button(new Rect(960, 80, 50, 50), enemyType2Texture))
+            Debug.Log("Clicked");
+    }
+}
---
+++ Assets/StructurePlacer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructurePlacer : MonoBehaviour {
+
+
+    public GameObject structurePrefab;
+    private GameObject structure;
+
+    private bool canPlaceStructure()
+    {
+        return structure == null;
+
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        if (canPlaceStructure())
+        {
+            structure = (GameObject)Instantiate(structurePrefab, transform.position, Quaternion.identity);
+            Debug.Log(transform.position);
+            Debug.Log(structure);
+
+        }
+    }
+}
